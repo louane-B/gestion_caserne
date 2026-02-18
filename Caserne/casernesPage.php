4b72912e826abd14b46da4a551b52e54ad362299
louane-B <louane31012004@gmail.com>
2026-02-18 14:52:58 -0500

    ajout de la liste de caserne

--- Caserne/casernesPage.php
+++ Caserne/casernesPage.php
@@ -10,9 +10,41 @@
      <?php
     include "../connexion.php";
     include "../menu.php";
+
+    $req = $pdo->prepare("SELECT id, nom, adresse, ville, province, code_postal, telephone FROM caserne ORDER BY id;");
+    $req->execute();
+    $tab = $req->fetchAll(PDO::FETCH_ASSOC);
     ?>
     <div class="contenu-page">
         <h1>La liste  des casernes de pompiers du Québec</h1>
+        <form method="POST">
+            <table>
+                <tr>
+                    <th>Nom</th>
+                    <th>Adresse</th>
+                    <th>Ville</th>
+                    <th>Province</th>
+                    <th>code postal</th>
+                    <th>Téléphone</th>
+                </tr>
+                    <?php
+                        for($i=0;$i<count($tab);$i++){
+                            echo "<tr>";
+                            echo "<td>" . $tab[$i]["nom"] . "</td>";
+                            echo "<td>" . $tab[$i]["adresse"] . "</td>";
+                            echo "<td>" . $tab[$i]["ville"] . "</td>";
+                            echo "<td>" . $tab[$i]["province"] . "</td>";
+                            echo "<td>" . $tab[$i]["code_postal"] . "</td>";
+                            echo "<td>" . $tab[$i]["telephone"] . "</td>";
+                        }
+                    ?>
+                </tr>
+            </table>
+            <input type="hidden" id="id_garderie" name="id_garderie">
+        </form>
+        <br/>
+        <br/>
+        <br/>
     </div>
 </body>
 </html>
