a38f617253e6868258bf22068b95122df24feaba
louane-B <louane31012004@gmail.com>
2026-02-18 13:14:29 -0500

    creation page CasernePage

---
+++ Caserne/casernesPage.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>casernes</title>
+</head>
+<body>
+    <h1>La liste  des casernes de pompiers du Québec</h1>
+    <?php
+    include "../connexion.php";
+    include "../menu.php";
+    ?>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,6 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Accueil</a></li>
+        <li><a href="Caserne/casernesPage.php">les casernes</a></li>
+    </ul>
+</nav>
