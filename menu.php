b2265e6c6709e58654ddf3c30165aa14e85ff6ca
louane-B <louane31012004@gmail.com>
2026-02-20 13:33:43 -0500

    ajout du style de la liste

--- menu.php
+++ menu.php
@@ -4,6 +4,6 @@
 <nav class="menu-vertical">
     <ul>
         <li><a href="../index.php">Accueil</a></li>
-        <li><a href="Caserne/casernesPage.php">les casernes</a></li>
+        <li><a href="../Caserne/casernesPage.php">les casernes</a></li>
     </ul>
 </nav>
