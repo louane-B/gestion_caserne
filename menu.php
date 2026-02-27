25784e72e1c02baec0abeddbd2a0eb18e9971f32
louane-B <louane31012004@gmail.com>
2026-02-27 13:36:34 -0500

    commentaire du menu.php

--- menu.php
+++ menu.php
@@ -1,10 +1,16 @@
 <head>
+    <!-- Feuille de style du menu -->
     <link rel="stylesheet" href="../style/menuStyle.css">
 </head>
+<!-- début du menu -->
 <nav class="menu-vertical">
     <ul>
+        <!-- lien vers la page d'accueil -->
         <li><a href="../index.php">Accueil</a></li>
+        <!-- lien vers la page de la liste des casernes -->
         <li><a href="../Caserne/casernesPage.php">les casernes</a></li>
+        <!-- lien vers la page de la liste des intervention -->
         <li><a href="../Intervention/interventionsPage.php">les Interventions</a></li>
     </ul>
 </nav>
+<!-- fin du menu -->
